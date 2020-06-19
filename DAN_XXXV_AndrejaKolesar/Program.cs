95f8d85477bf8c4c2a390e2f9d626be31b8ddb5c
KolesarAndreja <kolesar.andreja@gmail.com>
2020-06-19 14:51:56 +0200

    OutOfMemoryException in catch block added, comments added

--- DAN_XXXV_AndrejaKolesar/Program.cs
+++ DAN_XXXV_AndrejaKolesar/Program.cs
@@ -13,43 +13,61 @@ namespace DAN_XXXV_AndrejaKolesar
         static object obj = new object();
         static int numOfParticipant;
         static int number;
+
         static Task<Thread[]> t2 = new Task<Thread[]>(ThreadGenerator);
         static bool IsGuessed = false;
         public static Random random = new Random();
 
+        /// <summary>
+        /// Get user input about number of participants and desired hidden number
+        /// </summary>
         public static void UserInput()
         {
             Console.Write("Unesite broj učesnika: ");
             numOfParticipant = ValidNumber(int.MaxValue);
             Console.Write("Unesite broj između 1 i 100 koji će ostali učesnici pogađati: ");
             number = ValidNumber(100);
+            //start with t2
             t2.Start();
             Console.WriteLine("Broj učesnika: {0} \nOdabrani broj: {1}", numOfParticipant, number);
         }
 
+        /// <summary>
+        /// Generating thread for every participant with name in format Ucesnik_X
+        /// </summary>
+        /// <returns></returns>
         public static Thread[] ThreadGenerator()
         {
             Thread[] participants = new Thread[numOfParticipant];
-            for(int i = 0; i < numOfParticipant; i++)
+            try
             {
-                participants[i] = new Thread(GuessNumber)
+                for (int i = 0; i < numOfParticipant; i++)
                 {
-                    Name = string.Format("Učesnik_{0}", i + 1)
-                };
+                    participants[i] = new Thread(GuessNumber)
+                    {
+                        Name = string.Format("Učesnik_{0}", i + 1)
+                    };
+                }
             }
-
+            catch (OutOfMemoryException)
+            {
+                Console.WriteLine("Uneli ste previse igraca. Sledeci put pokusajte manje.");
+            }
             return participants;
         }
 
+        /// <summary>
+        /// Participant guesses hidden number until he or some other participant does not find it.
+        /// </summary>
         public static void GuessNumber()
         {
-            
             int n=0;
             do
             {
+                //only one participant has access to guess number in time
                 lock (obj)
                 {
-                    int userNumber = random.Next(0, 101);
+                    int userNumber = random.Next(1, 101);
                     if (!IsGuessed)
                     {
                         if (userNumber == number)
@@ -68,6 +86,7 @@ namespace DAN_XXXV_AndrejaKolesar
                         }
                     }
                 }
+                //sleep for 100ms
                 Thread.Sleep(100);
             } while (n != number && !IsGuessed);
         }
@@ -80,7 +99,7 @@ namespace DAN_XXXV_AndrejaKolesar
             string s = Console.ReadLine();
             int Num;
             bool b = Int32.TryParse(s, out Num);
-            while (!b || Num < 0 || Num > limit)
+            while (!b || Num <= 0 || Num > limit)
             {
                 Console.Write("Pogrešan unos. Ponovite: ");
                 s = Console.ReadLine();
@@ -96,11 +115,14 @@ namespace DAN_XXXV_AndrejaKolesar
             t1.Join();
             t2.Wait();
             Thread[] generatedThreads = t2.Result;
+      
             foreach (var t in generatedThreads)
             {
                 t.Start();
             }
 
+        
+   
             foreach (var t in generatedThreads)
             {
                 t.Join();
