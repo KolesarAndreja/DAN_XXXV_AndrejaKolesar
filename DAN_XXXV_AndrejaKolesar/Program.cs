d4ed912b9a4f67bb072f99f545cc66cb62c33666
KolesarAndreja <kolesar.andreja@gmail.com>
2020-06-19 13:27:17 +0200

    GuessNumber implemented, locker obj added, NumberGuessingSimulator implemented

--- DAN_XXXV_AndrejaKolesar/Program.cs
+++ DAN_XXXV_AndrejaKolesar/Program.cs
@@ -9,16 +9,20 @@ namespace DAN_XXXV_AndrejaKolesar
 {
     class Program
     {
+        //locker
+        static object obj = new object();
+        static object obj2 = new object();
         static int numOfParticipant;
         static int number;
         static Thread t1 = new Thread(UserInput);
         static Task<Thread[]> t2 = new Task<Thread[]>(ThreadGenerator);
+        static bool IsGuessed = false;
 
         public static void UserInput()
         {
-            Console.WriteLine("Unesite broj učesnika: ");
+            Console.Write("Unesite broj učesnika: ");
             numOfParticipant = ValidNumber(int.MaxValue);
-            Console.WriteLine("Unesite broj između 1 i 100 koji će ostali učesnici pogađati: ");
+            Console.Write("Unesite broj između 1 i 100 koji će ostali učesnici pogađati: ");
             number = ValidNumber(100);
             t2.Start();
             Console.WriteLine("Broj učesnika: {0} \nOdabrani broj: {1}", numOfParticipant, number);
@@ -40,11 +44,38 @@ namespace DAN_XXXV_AndrejaKolesar
 
         public static void GuessNumber()
         {
+            Random random = new Random();
+            int n=0;
+            do
+            {
+                int userNumber = random.Next(0, 101);
+                lock (obj)
+                {
+
+                    if (!IsGuessed)
+                    {
+                        if (userNumber == number)
+                        {
+                            Console.WriteLine("{0} je pobedio, a traženi broj je bio {1}. ", Thread.CurrentThread.Name, number);
+                            IsGuessed = true;
+                        }
+                        else
+                        {
+                            Console.Write("{0} je pokusao da pogodi skriveni broj, njegov izbor je bio {1}. ", Thread.CurrentThread.Name, userNumber);
+                            if (userNumber % 2 == number % 2)
+                            {
+                                Console.Write("{0} je pogodio parnost broja!", Thread.CurrentThread.Name);
+                            }
+                            Console.WriteLine();
+                        }
+                    }
+                }
+            } while (n != number && !IsGuessed);
 
         }
 
         /// <summary>
-        /// Return valid positive number in range (0,limit] number
+        /// Return valid positive number in range [1,limit] number
         /// </summary>
         public static int ValidNumber(int limit)
         {
@@ -65,6 +96,16 @@ namespace DAN_XXXV_AndrejaKolesar
             t1.Start();
             t1.Join();
             t2.Wait();
+            Thread[] generatedThreads = t2.Result;
+            foreach(var t in generatedThreads)
+            {
+                t.Start();
+            }
+
+            foreach(var t in generatedThreads)
+            {
+                t.Join();
+            }
         }
 
         static void Main(string[] args)
